2fca0ec264a1b9a3860b3661398328f8a59974a0
chloeraats4 <craats@carroll.edu>
2024-05-01 18:28:23 -0600

    format

---
+++ js/final.js
@@ -0,0 +1,8 @@
+var navLinks = document.getElementById("navlinks");
+
+function showmenu() {
+  navLinks.style.display = "block";
+}
+function hidemenu() {
+  navLinks.style.display = "none";
+}
--- trial.js
+++
@@ -1,9 +0,0 @@
-
-var navLinks = document.getElementById("navlinks");
-
-function showmenu(){
-    navLinks.style.display = "block";
-}
-function hidemenu(){
-    navLinks.style.display="none";
-}
